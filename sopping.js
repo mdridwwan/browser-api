7d55f80ebb26b32af5d6bd299ab88393298aaa39
Md Ridwan <72821054+mdridwwan@users.noreply.github.com>
2021-09-08 21:21:28 +0600

    last update

--- sopping.js
+++ sopping.js
@@ -1,53 +1,53 @@
-const displayLocalStrorageCart = () => {
-    const cart = getCart();
-    for (const name in cart) {
-        displayItem(name)
-    }
-};
+// const displayLocalStrorageCart = () => {
+//     const cart = getCart();
+//     for (const name in cart) {
+//         displayItem(name)
+//     }
+// };
 
-const addItem = () => {
-    const nameFiled = document.getElementById('product-name');
-    const name = nameFiled.value;
-    if (!name) {
-        return;
-    }
-    //display add the ui
-    displayItem(name)
-        // add local storage
-    addProductToCart(name);
+// const addItem = () => {
+//     const nameFiled = document.getElementById('product-name');
+//     const name = nameFiled.value;
+//     if (!name) {
+//         return;
+//     }
+//     //display add the ui
+//     displayItem(name)
+//         // add local storage
+//     addProductToCart(name);
 
 
-    nameFiled.value = '';
-}
+//     nameFiled.value = '';
+// }
 
-const displayItem = (name) => {
-    const ul = document.getElementById('addItem');
-    const li = document.createElement('li');
-    li.innerText = name;
-    ul.appendChild(li);
-}
+// const displayItem = (name) => {
+//     const ul = document.getElementById('addItem');
+//     const li = document.createElement('li');
+//     li.innerText = name;
+//     ul.appendChild(li);
+// }
 
-const getCart = () => {
-    const cart = localStorage.getItem('cart')
-    let cartObject;
-    if (cart) {
-        cartObject = JSON.parse(cart);
-    } else {
-        cartObject = {};
-    }
-    return cartObject;
-}
-const addProductToCart = name => {
-    const cart = getCart();
-    if (cart[name]) {
-        cart[name] += 1;
-    } else { cart[name] = 1; }
-    console.log(cart);
-    const cartStringified = JSON.stringify(cart);
-    localStorage.setItem('cart', cartStringified);
-}
-const placeOrder = () => {
-    document.getElementById('addItem').textContent = '';
-    localStorage.removeItem('cart');
-}
-displayLocalStrorageCart();
+// const getCart = () => {
+//     const cart = localStorage.getItem('cart')
+//     let cartObject;
+//     if (cart) {
+//         cartObject = JSON.parse(cart);
+//     } else {
+//         cartObject = {};
+//     }
+//     return cartObject;
+// }
+// const addProductToCart = name => {
+//     const cart = getCart();
+//     if (cart[name]) {
+//         cart[name] += 1;
+//     } else { cart[name] = 1; }
+//     console.log(cart);
+//     const cartStringified = JSON.stringify(cart);
+//     localStorage.setItem('cart', cartStringified);
+// }
+// const placeOrder = () => {
+//     document.getElementById('addItem').textContent = '';
+//     localStorage.removeItem('cart');
+// }
+// displayLocalStrorageCart();
---
+++ sumarry.js
@@ -0,0 +1,13 @@
+alert('');
+confirm('are you single?');
+prompt('name');
+// location.reload();
+// location.href = '';
+1 // location.hash = (id diye jaity parbo je kono jaigai)
+// history.back ();
+// history.go();
+
+
+//cookies
+// sessionStorage vs localStorage
+// cookies vs sassionStorage vs localStorage
